6a92d12da5c926fc0ad6c0d546d8ebe5d3a36432
Zahrajsi.net <nextikczcz@gmail.com>
2022-01-09 10:07:00 +0100

    Version 2.2
    Creating a class for spigot and waterfall also added "NON_SUPPORTED_INSTANCE" - Which is an installer for "#.jar" spigot and waterfall created as a group, which are then called by the function

--- main.bash
+++ main.bash
@@ -88,10 +88,12 @@ clear
 
 # Variables
     Variables(){
-        SCRIPT_VERSION="2.0"
+        SCRIPT_VERSION="2.2"
         SUPPORT_LINK="https://zahrajsi.net"
-        SPIGOT_LINK=""
-        WATERFALL_LINK=""
+        SPIGOT_VER="1.18.1"
+        WATERFALL_VER="1.18.1"
+        SPIGOT_LINK="https://papermc.io/ci/job/$SPIGOT_VER/lastStableBuild/artifact/paperclip.jar"
+        WATERFALL_LINK="https://papermc.io/ci/job/$WATERFALL_VER/lastStableBuild/artifact/waterfall.jar"
         SPIGOT_FILE="server.jar"
         WATERFALL_FILE="waterfall.jar"
         BUKKIT_FILE="bukkit.jar"
@@ -153,20 +155,7 @@ clear
 }
 #
 
-# RUN_CODE
-    RUN_CODE(){
-        check_distro
-        Variables
-        colour
 
-        errors(){
-            for t in "${error[@]}"; do
-                echo "$t"
-            done
-        }
-    }  
-#
-RUN_CODE
 
 # VAR SPIGOT
     vars_spigot(){
@@ -179,9 +168,34 @@ RUN_CODE
     }
 #
 
+# VAR WATERAFLL
+    vars_waterfall(){
+    W_PROPERTIES="config.yml" 
+    W_D_FILE="motd_watefall" 
+    ICON="server-icon.png" 
+    W_Login_D="plugins/AuthMeBungee"
+    W_Login_tar="authme-conf-bungee.tar.gz"
+    W_S_DIR="plugins"
+    }
+#
+
 # Initial code
     main() {
     # WELCOME MESS.
+    # RUN_CODE
+        RUN_CODE(){
+            check_distro
+            Variables
+            colour
+
+            errors(){
+                for t in "${error[@]}"; do
+                    echo "$t"
+                done
+            }
+        }  
+    #
+    RUN_CODE
     print_brake 47
     echo -e "* ${YELLOW} Install Script AutoConfigNODE ${reset}"
     low_echo "Owner: F0cus"
@@ -301,118 +315,206 @@ RUN_CODE
 
     # SPIGOT INSTANCE
         if [ "$SERVER_LINK" == "YES" ]; then
-            if [ "$INSTANCE" == "SPIGOT" ]; then
-                vars_spigot
-                print_info "A spigot was detected!"
-                echo -e " ${BLUE}* ${reset}A spigot was detected!"
-                echo ""
-                low_echo "1. MOTD"
-                low_echo "2. Server-Icon"
-                low_echo "3. Login Plugin (AuthMe)"
-                low_echo "4. Another Plugins (INSTALLER)"
-                echo -e ""
-                print_info "> Inialize installer for spigot"
+            install_spigot(){
+                if [ "$INSTANCE" == "SPIGOT" ]; then
+                    vars_spigot
+                    print_info "A spigot was detected!"
+                    echo -e " ${BLUE}* ${reset}A spigot was detected!"
+                    echo ""
+                    low_echo "1. MOTD"
+                    low_echo "2. Server-Icon"
+                    low_echo "3. Login Plugin (AuthMe)"
+                    low_echo "4. Another Plugins (INSTALLER)"
+                    echo -e ""
+                    print_info "> Inialize installer for spigot"
 
-                # Install MOTD
-                touch "$PROPERTIES" #Make sure file exist
-                if [ -f "$PROPERTIES" ]; then
-                    #Check for Motd
-                    if grep -q Minecraft "$PROPERTIES"; then
-                        TMP_GREP="UNSET"
-                    elif grep -q ZAHRAJSI "$PROPERTIES"; then
-                        TMP_GREP="SET"
-                    else
-                        TMP_GREP="USER"
-                    fi
-                fi
-                if [ "$TMP_GREP" == "USER" ]; then
-                    print_info "> MOTD is already set, by the previous user!"
-                elif [ "$TMP_GREP" == "SET" ]; then
-                    print_info "> MOTD already set! Skipping.."
-                elif [ "$TMP_GREP" == "UNSET" ]; then
-                    print_warning "> Creating the necessary files.. for $INSTANCE"
-                    touch "$PROPERTIES"
-                    print_warning "> Downloading MOTD from an external server.. for $INSTANCE"
-                    if [ ! -f "$D_FILE" ]; then
-                        wget -q "https://zahrajsi.net/amp/assets/installer/motd_spigot"
-                        cat "$D_FILE" > "$PROPERTIES"
-                        print_success "> MOTD set successfully!"
-                        MOTD_SET="true"
-                        rm -r "$D_FILE"
-                    fi
-                fi
-
-                # Install Server-Icon
-                if [ -f "$ICON" ]; then
-                    print_info "> The $ICON icon has already been set!"
-                else
-                    print_info "> Set Icon"
-                    wget -q "https://zahrajsi.net/amp/assets/pngs/server-icon.png"
-                    low_echo "> Downloading Server Icon from an external server.. for $INSTANCE"
-                    print_success "> Icon set successfully!"
-                fi
-
-                # Install Login Plugin
-                if [ -d "$Login_D" ] && [ ! "$AUTHME" == "TRUE" ]; then
-                    print_info "> Authme plugin is already installed.."
-                else
-                    print_info "> Installing AuthMe"
-                    if [ ! -f "$Login_tar" ]; then
-                        low_echo "The $Login_tar file does not exist, they are downloading and unpacking"
-                        if [ ! -f "$S_DIR" ]; then
-                            mkdir "$S_DIR"
-                            low_echo "Making folder!"
+                    # Install MOTD
+                    touch "$PROPERTIES" #Make sure file exist
+                    if [ -f "$PROPERTIES" ]; then
+                        #Check for Motd
+                        if grep -q Minecraft "$PROPERTIES"; then
+                            TMP_GREP="UNSET"
+                        elif grep -q ZAHRAJSI "$PROPERTIES"; then
+                            TMP_GREP="SET"
+                        else
+                            TMP_GREP="USER"
                         fi
-                    wget -q "https://zahrajsi.net/amp/assets/installer/authme-conf.tar.gz"
-                    low_echo "Unzipping the authme-conf.tar.gz"
-                    sleep 2
-                    tar -zxf authme-conf.tar.gz --directory plugins
-                    rm -r authme-conf.tar.gz
-                    print_success "> AuthMe set successfully!"
-                    AUTHME="TRUE"
                     fi
-                fi
+                    if [ "$TMP_GREP" == "USER" ]; then
+                        print_info "> MOTD is already set, by the previous user!"
+                    elif [ "$TMP_GREP" == "SET" ]; then
+                        print_info "> MOTD already set! Skipping.."
+                    elif [ "$TMP_GREP" == "UNSET" ]; then
+                        print_warning "> Creating the necessary files.. for $INSTANCE"
+                        touch "$PROPERTIES"
+                        print_warning "> Downloading MOTD from an external server.. for $INSTANCE"
+                        if [ ! -f "$D_FILE" ]; then
+                            wget -q "https://zahrajsi.net/amp/assets/installer/motd_spigot"
+                            cat "$D_FILE" > "$PROPERTIES"
+                            print_success "> MOTD set successfully!"
+                            MOTD_SET="true"
+                            rm -r "$D_FILE"
+                        fi
+                    fi
 
-                # Install Another Plugin
-                if [ "$A_PLUGIN" != "TRUE" ]; then
+                    # Install Server-Icon
+                    if [ -f "$ICON" ]; then
+                        print_info "> The $ICON icon has already been set!"
+                    else
+                        print_info "> Set Icon"
+                        wget -q "https://zahrajsi.net/amp/assets/pngs/server-icon.png"
+                        low_echo "> Downloading Server Icon from an external server.. for $INSTANCE"
+                        print_success "> Icon set successfully!"
+                    fi
+
+                    # Install Login Plugin
+                    if [ -d "$Login_D" ] && [ ! "$AUTHME" == "TRUE" ]; then
+                        print_info "> Authme plugin is already installed.."
+                    else
+                        print_info "> Installing AuthMe"
+                        if [ ! -f "$Login_tar" ]; then
+                            low_echo "The $Login_tar file does not exist, they are downloading and unpacking"
+                            if [ ! -f "$S_DIR" ]; then
+                                mkdir "$S_DIR"
+                                low_echo "Making folder!"
+                            fi
+                        wget -q "https://zahrajsi.net/amp/assets/installer/authme-conf.tar.gz"
+                        low_echo "Unzipping the authme-conf.tar.gz"
+                        sleep 2
+                        tar -zxf authme-conf.tar.gz --directory plugins
+                        rm -r authme-conf.tar.gz
+                        print_success "> AuthMe set successfully!"
+                        AUTHME="TRUE"
+                        fi
+                    fi
+
+                    # Install Another Plugin
+                    if [ "$A_PLUGIN" != "TRUE" ]; then
+                        echo -e ""
+                        print_brake 45
+                        print_warning "Installing Other Plugins"
+                        print_brake 45
+                        echo -e ""
+                        mycommand
+                        skript
+                        residence
+                    fi
+                fi 
+            }
+            install_spigot
+            # END OF SPIGOT 
+
+             install_waterfall(){
+                if [ "$INSTANCE" == "WATERFALL" ]; then
+                    vars_waterfall
+                    print_info "A waterfall was detected!"
+                    echo -e " ${BLUE}* ${reset}A waterfall was detected!"
+                    echo ""
+                    low_echo "1. MOTD"
+                    low_echo "2. Server-Icon"
+                    low_echo "3. Login Plugin (AuthMeBungee)"
                     echo -e ""
-                    print_brake 45
-                    print_warning "Installing Other Plugins"
-                    print_brake 45
-                    echo -e ""
-                    mycommand
-                    skript
-                    residence
-                #     PS3='Vyber možnost:  '
-                #     options=("MyCommand" "Skript" "Residence" "Ukončit")
-                #     select opt in "${options[@]}"
-                #     do
-                #         case $opt in
-                #             "MyCommand")
-                #                 set -e
-                #                 echo ""
-                #                 mycommand
-                #                 ;;
-                #             "Skript")
-                #                 set -e
-                #                 echo ""
-                #                 skript
-                #                 ;;
-                #             "Residence")
-                #                 set -e
-                #                 echo ""
-                #                 residence
-                #                 ;;
-                #             "Ukončit")
-                #                 break 1
-                #                 ;;
-                #             *) echo "invalid option $REPLY";;
-                #         esac
-                #     done
-                fi
+                    print_info "> Inialize installer for waterafll"
 
+                    # Install MOTD
+                    touch "$W_PROPERTIES" #Make sure file exist
+                    if [ -f "$W_PROPERTIES" ]; then
+                        #Check for Motd
+                        if grep -q Minecraft "$W_PROPERTIES"; then
+                            TMP_GREP="UNSET"
+                        elif grep -q ZAHRAJSI "$W_PROPERTIES"; then
+                            TMP_GREP="SET"
+                        else
+                            TMP_GREP="USER"
+                        fi
+                    fi
+                    if [ "$TMP_GREP" == "USER" ]; then
+                        print_info "> MOTD is already set, by the previous user!"
+                    elif [ "$TMP_GREP" == "SET" ]; then
+                        print_info "> MOTD already set! Skipping.."
+                    elif [ "$TMP_GREP" == "UNSET" ]; then
+                        print_warning "> Creating the necessary files.. for $INSTANCE"
+                        touch "$W_PROPERTIES"
+                        print_warning "> Downloading MOTD from an external server.. for $INSTANCE"
+                        if [ ! -f "$W_D_FILE" ]; then
+                            wget -q "https://zahrajsi.net/amp/assets/installer/$W_D_FILE"
+                            cat "$D_FILE" > "$W_PROPERTIES"
+                            print_success "> MOTD set successfully!"
+                            MOTD_SET="true"
+                            rm -r "$W_D_FILE"
+                        fi
+                    fi
 
-            fi # END OF SPIGOT
+                    # Install Server-Icon
+                    if [ -f "$ICON" ]; then
+                        echo -e ""
+                        print_info "> The $ICON icon has already been set!"
+                    else
+                        echo -e ""
+                        print_info "> Set Icon"
+                        wget -q "https://zahrajsi.net/amp/assets/pngs/server-icon.png"
+                        low_echo "> Downloading Server Icon from an external server.. for $INSTANCE"
+                        print_success "> Icon set successfully!"
+                    fi
+
+                    # Install Login Plugin
+                    if [ -d "$W_Login_D" ] && [ ! "$AUTHMEBUNGEE" == "TRUE" ]; then
+                        print_info "> Authme plugin is already installed.."
+                    else
+                        print_info "> Installing AuthMeBungee"
+                        if [ ! -f "$W_Login_tar" ]; then
+                            low_echo "The $Login_tar file does not exist, they are downloading and unpacking"
+                            if [ ! -f "$W_S_DIR" ]; then
+                                mkdir "$W_S_DIR"
+                                low_echo "Making folder!"
+                            fi
+                        wget -q "https://zahrajsi.net/amp/assets/installer/$W_Login_tar"
+                        low_echo "Unzipping the $W_Login_tar"
+                        sleep 2
+                        tar -zxf "$W_Login_tar" --directory plugins
+                        rm -r "$W_Login_tar"
+                        print_success "> AuthMeBungee set successfully!"
+                        AUTHMEBUNGEE="TRUE"
+                        fi
+                    fi
+                fi 
+            }
+            install_waterfall
+            # END OF WATERFALL 
+
+            if [ "$INSTANCE" == "NON_SUPPORTED_INSTANCE" ]; then
+                print_error "We couldn't find any installation, do you want to install a new instance?"
+                echo -e ""
+                PS3='Vyber možnost:  '
+                options=("Paper-Spigot" "Waterfall" "Ukončit")
+                select opt in "${options[@]}"
+                do
+                    case $opt in
+                        "Paper-Spigot")
+                            set -e
+                            echo ""
+                            low_echo "Downloading Spigot $SPIGOT_VER".
+                            wget -q "$SPIGOT_LINK" -O "$SPIGOT_FILE"
+                            find_instance
+                            install_spigot
+                            break 1
+                            ;;
+                        "Waterfall")
+                            set -e
+                            low_echo "Downloading Waterfall $WATERFALL_VER".
+                            wget -q "$WATERFALL_LINK" -O "$WATERFALL_FILE"
+                            find_instance
+                            install_waterfall
+                            break 1
+                            ;;
+                        "Ukončit")
+                            break
+                            ;;
+                        *) echo "invalid option $REPLY";;
+                    esac
+                done
+            fi
+        #
         else
             print_error "Webpage for download dependents in unavailable"
         fi # END URL LINK
@@ -421,10 +523,15 @@ RUN_CODE
     }
 #
 main
-echo "$SERVER_LINK"
-echo "$PROPERTIES"
-echo "$INSTANCE"
-echo "$MOTD_SET"
-echo "$TMP_GREP"
-echo "$AUTHME"
-errors
+debug(){
+    echo -e ""
+    print_error "DEBUG MENU"
+    echo -e ""
+    echo "$SERVER_LINK"
+    echo "$PROPERTIES"
+    echo "$INSTANCE"
+    echo "$MOTD_SET"
+    echo "$TMP_GREP"
+    echo "$AUTHME"
+    errors
+    }
